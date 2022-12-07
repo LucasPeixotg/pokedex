42b132531cc2a4a40ed76de237391d189eb6faa0
Lucas Peixoto Goncalves <peixoto@pop-os.localdomain>
2022-12-07 09:35:41 -0300

    Project started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { 
+  Container,
+  TextField,
+  Avatar,
+  Paper,
+  Grid,
+  Typography
+} from '@mui/material';
 
-function App() {
+const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+
+const getUsefulPropertiesOfPokemon = (data) => {
+  return {
+    "id": data.id,
+    "name": data.name[0].toUpperCase() + data.name.substring(1),
+    "sprite_front": data.sprites.front_default,
+    "sprite_back": data.sprites.back_default,
+    "abilities": data.abilities,
+    "weight": data.weight,
+    "heigth": data.heigth
+  }
+}
+
+const App = () => {
+  const [ pokemon, setPokemon ] = useState({})
+  
+  useEffect(() => {
+    searchPokemon('nidorino')
+  }, []);
+
+  const searchPokemon = async (pokemonName) => {
+    const response = await fetch(`${API_URL}/${pokemonName}`)
+    const data = await response.json()
+  
+    setPokemon(getUsefulPropertiesOfPokemon(data))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <TextField id="pokemonNameInput" label="Pokemon Name" variant="outlined" />
+      { pokemon? (
+        <Paper>
+          <Typography align="center" variant="h1">{ pokemon["name"] }</Typography>
+          <Grid container display="flex" justifyContent="center">
+            <Grid item xs={2}>
+              <Avatar
+                alt="front"
+                src={ pokemon["sprite_front"] }
+                sx={{ width: 200, height: 200 }}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <Avatar
+                alt="back"
+                src={ pokemon["sprite_back"] }
+                sx={{ width: 200, height: 200 }}
+              />
+            </Grid>
+          </Grid>
+        </Paper>
+      ) : (
+        <h1>Couldn't find any pokemon with this name</h1>
+      )}
+    </Container>
   );
 }
 
